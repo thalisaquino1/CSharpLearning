eb75735dcc20c074d1fd2fbfb13d60aeca838b81
Thalis Aquino <xandeDev@outlook.com>
2026-05-26 23:00:28 -0300

    Adicione README.md.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿int n1, n2, n3, sum;
+
+Console.WriteLine("Enter the first number: ");
+n1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the second number: ");
+n2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the third number: ");
+n3 = int.Parse(Console.ReadLine()!);
+
+sum = n1 + n2 + n3;
+
+Console.WriteLine($"The sum is {sum}");
