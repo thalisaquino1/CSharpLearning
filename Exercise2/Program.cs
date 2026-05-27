fbfdf6775160fee2bed9f5d15239385b13966ab0
Thalis Aquino <xandeDev@outlook.com>
2026-05-26 23:03:57 -0300

    Add files via upload

---
+++ Exercise2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n1, doubl, triple, quadruple;
+
+Console.WriteLine("Enter a number: ");
+n1 = int.Parse(Console.ReadLine()!);
+
+doubl = n1 * 2;
+triple = n1 * 3;
+quadruple = n1 * 4;
+
+Console.WriteLine($"Double is {doubl}");
+Console.WriteLine($"Triple is {triple}");
+Console.WriteLine($"Quadruple is {quadruple}");
